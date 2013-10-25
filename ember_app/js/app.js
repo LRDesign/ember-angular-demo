767f06d5fbee9a0028811120edd506473fdb2548
Hannah Howard <hannah@hannahhoward.net>
2013-10-24 17:37:38 -0700

    Remove Firebase

--- ember_app/js/app.js
+++ ember_app/js/app.js
@@ -1,6 +1,9 @@
 App = Ember.Application.create();
 
-App.TicketController = Ember.ObjectController.extend({
+App.Ticket = Ember.Object.extend({
+
+  name: null,
+  priority: null,
 
   slug: function() {
     return this.get('name')
@@ -19,15 +22,30 @@ App.TicketController = Ember.ObjectController.extend({
 App.Router.map(function() {
   // put your routes here
   this.resource("tickets", function() {
-    //this.route("edit", {path: "/:ticket_slug"});
+    this.route("edit", {path: "/:ticket_slug"});
   });
 });
 
 App.TicketsRoute = Ember.Route.extend({
   model: function() {
-    return EmberFire.Array.create({
-      ref: new Firebase("https://ember-angular-demo.firebaseio.com/tickets")
-    });
+    return [
+      App.Ticket.create({
+        name: "Write the presentation",
+        priority: 1,
+      }),
+      App.Ticket.create({
+        name: "Write the demo app",
+        priority: 2
+      }),
+      App.Ticket.create({
+        name: "Give the presentation",
+        priority: 2
+      }),
+      App.Ticket.create({
+        name: "Understand what I'm talking about",
+        priority: 5
+      })
+    ];
   }
 });
 
@@ -40,32 +58,28 @@ App.TicketsIndexRoute = Ember.Route.extend({
 
 App.TicketsIndexController = Ember.ArrayController.extend({
 
-  itemController: 'ticket',
-
   actions: {
     addItem: function() {
       name = this.get("name")
       priority = this.get("priority")
-      slug = name.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'');
-      ticket = {
+      ticket =  App.Ticket.create({
         name: name,
-        priority: priority,
-        slug: slug
-      }
+        priority: priority
+      });
       this.pushObject(ticket);
     }
   }
 });
 
 App.TicketsEditRoute = Ember.Route.extend({
-  /*model: function(params) {
+  model: function(params) {
     return this.modelFor('tickets').find('slug', params.ticket_slug)
   },
 
   serialize: function(model) {
     // this will make the URL `/posts/foo-post`
-    return { ticket_slug: model.slug };
-  }*/
+    return { ticket_slug: model.get('slug') };
+  }
 });
 
 App.IndexRoute = Ember.Route.extend({
